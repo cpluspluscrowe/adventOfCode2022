4c2164d46afe1fe7d1db9ebefee4b4830c20df2a
Chad Crowe <ccrowe@unomaha.edu>
2022-12-01 08:00:03 -0600

    formatting

--- day1/day1.scala
+++ day1/day1.scala
@@ -1,35 +1,40 @@
 import scala.io.Source._
 import scala.collection.mutable.ListBuffer
 
-def sumCalories(group: ListBuffer[Int]): Int = {
-  var sum = 0
-  for(item <- group){
-    sum = sum + item
-  }
-  sum
-}
+object CalorieHungryElves {
+  val lines = loadLines()
+  val groups = generateGroups(lines)
+  val personCalories = groups.map(sumCalories).sorted(Ordering.Int.reverse)
+  val topThree = personCalories(0) + personCalories(1) + personCalories(2)
 
-def generateGroups(lines: List[String]): ListBuffer[ListBuffer[Int]] = {
-  var groups: ListBuffer[ListBuffer[Int]] = ListBuffer()
-  var currentList: ListBuffer[Int] = ListBuffer()
-  for(line <- lines){
-    line match
-    case "" => {
-      groups += currentList
-      currentList = ListBuffer()
+  def loadLines(): List[String] = {
+    val source = fromFile("input.txt")
+    val lines = source.getLines.toList
+    source.close
+    lines
+  }
+
+  def sumCalories(group: ListBuffer[Int]): Int = {
+    var sum = 0
+    for (item <- group) {
+      sum = sum + item
     }
-    case _ => currentList += line.toInt
+    sum
   }
-  groups
+
+  def generateGroups(lines: List[String]): ListBuffer[ListBuffer[Int]] = {
+    var groups: ListBuffer[ListBuffer[Int]] = ListBuffer()
+    var currentList: ListBuffer[Int] = ListBuffer()
+    for (line <- lines) {
+      line match
+        case "" => {
+          groups += currentList
+          currentList = ListBuffer()
+        }
+        case _ => currentList += line.toInt
+    }
+    groups
+  }
 }
 
-val source = fromFile("input.txt")
-val lines = source.getLines.toList
-source.close
-val groups = generateGroups(lines)
-val personCalories = groups.map(sumCalories).sorted(Ordering.Int.reverse)
-//val mostCaloriesPerPerson = personCalories.max
-val topThree = personCalories(0) + personCalories(1) + personCalories(2)
-
-
-
+CalorieHungryElves.topThree
