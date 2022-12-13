1b0d12861734ca8b87e2f33496262be5da318ecf
Chad Crowe <ccrowe@unomaha.edu>
2022-12-13 14:55:13 -0600

    reformatting

--- day7/directorySpace.scala
+++ day7/directorySpace.scala
@@ -5,6 +5,26 @@ val path = "./input.txt"
 val source = fromFile(path)
 val lines = source.getLines.toList
 source.close
+val types = lines.map(LineParser.classifyLines)
+val both = lines zip types
+val topDirectory = Directory("/")
+val space = 70000000 - currentDirectory.getFileSize()
+val answer = printStats(currentDirectory)
+var currentDirectory: Directory = topDirectory
+
+def printStats(directory: Directory): Int = {
+  val dirSize = directory.getFileSize()
+  var smallest = 70000001
+  val toBeFreed = space + dirSize
+  if(toBeFreed >= 30000000){
+    println(s"${directory.name}: $dirSize")
+    smallest = (toBeFreed).min(smallest)
+  }
+  for(child <- directory.children){
+    smallest = smallest.min(printStats(child))
+  }
+  smallest
+}
 
 sealed trait LineType(line: String) {
   val classification = "nothing"
@@ -18,6 +38,36 @@ class Command(line: String) extends LineType(line: String) {
   if(spl.size == 3){
     where = spl(2)
   }
+}
+for((line, lineType) <- both){
+  if(lineType.isInstanceOf[Command]){
+    val instance = lineType.asInstanceOf[Command]
+    if(instance.command.equals("cd")){
+      if(instance.where.equals("..")){
+        currentDirectory = currentDirectory.parentDirectory
+      }else if(instance.where.equals("/")){
+        currentDirectory = topDirectory
+      }else{
+        currentDirectory = currentDirectory.getOrCreateChild(instance.where)
+      }
+    }else if(instance.command.equals("ls")){
+      // grab the files later
+    }else throw new Exception(s"Invalid command: ${instance.command}")
+  }else if(lineType.isInstanceOf[File]){
+    val instance = lineType.asInstanceOf[File]
+    val file = File(line)
+    currentDirectory.files += file
+  }else if(lineType.isInstanceOf[Directory]){
+    val instance = lineType.asInstanceOf[Directory]
+    val directory = Directory(instance.name, currentDirectory)
+    currentDirectory.children += directory
+  }
+}
+
+
+
+while(currentDirectory.parentDirectory != null){
+  currentDirectory = currentDirectory.parentDirectory
 }
 
 class Directory(line: String, val parentDirectory: Directory = null) {
@@ -54,6 +104,15 @@ class File(line: String) extends LineType(line: String) {
 
 object LineParser {
 
+def classifyLines[A <: LineType](line: String): Any = {
+  if(LineParser.isCommand(line)) return Command(line)
+  else if(LineParser.isDir(line)) return Directory(line)
+  else if(LineParser.isFile(line)) return File(line)
+  else {
+    throw new Exception(s"This $line should not be reached")
+  }
+}
+
 def isCommand(line: String) : Boolean = {
   val spl = line.split(" ")
   spl(0) match {
@@ -66,74 +125,12 @@ def isDir(line: String): Boolean = {
   return line.split(" ")(0) == "dir"
 }
 
+// now let's also populate data about the command in the class instance.
+
 def isFile(line: String): Boolean = {
   if line.split(" ")(0).toIntOption.isEmpty then false else true
 }
-
-// now let's also populate data about the command in the class instance.
-
-def classifyLines[A <: LineType](line: String): Any = {
-  if(LineParser.isCommand(line)) return Command(line)
-  else if(LineParser.isDir(line)) return Directory(line)
-  else if(LineParser.isFile(line)) return File(line)
-  else {
-    throw new Exception(s"This $line should not be reached")
-  }
 }
-}
-
-val types = lines.map(LineParser.classifyLines)
-val both = lines zip types
-val topDirectory = Directory("/")
-var currentDirectory: Directory = topDirectory
-for((line, lineType) <- both){
-  if(lineType.isInstanceOf[Command]){
-    val instance = lineType.asInstanceOf[Command]
-    if(instance.command.equals("cd")){
-      if(instance.where.equals("..")){
-        currentDirectory = currentDirectory.parentDirectory
-      }else if(instance.where.equals("/")){
-        currentDirectory = topDirectory
-      }else{
-        currentDirectory = currentDirectory.getOrCreateChild(instance.where)
-      }
-    }else if(instance.command.equals("ls")){
-      // grab the files later
-    }else throw new Exception(s"Invalid command: ${instance.command}")
-  }else if(lineType.isInstanceOf[File]){
-    val instance = lineType.asInstanceOf[File]
-    val file = File(line)
-    currentDirectory.files += file
-  }else if(lineType.isInstanceOf[Directory]){
-    val instance = lineType.asInstanceOf[Directory]
-    val directory = Directory(instance.name, currentDirectory)
-    currentDirectory.children += directory
-  }
-}
-
-
-
-while(currentDirectory.parentDirectory != null){
-  currentDirectory = currentDirectory.parentDirectory
-}
-
-val space = 70000000 - currentDirectory.getFileSize()
-
-def printStats(directory: Directory): Int = {
-  val dirSize = directory.getFileSize()
-  var smallest = 70000001
-  val toBeFreed = space + dirSize
-  if(toBeFreed >= 30000000){
-    println(s"${directory.name}: $dirSize")
-    smallest = (toBeFreed).min(smallest)
-  }  
-  for(child <- directory.children){
-    smallest = smallest.min(printStats(child))
-  }
-  smallest
-}
-
-val answer = printStats(currentDirectory)
 println(answer - space)
 
 
